6b7409f5306c9ea3847132f48bc77394dcf200b9
左岸清河 <zuoanqh@live.com>
2015-08-16 21:50:33 +0800

    Setting up projects, plus simple window's layout
    no functionality implemented.

---
+++ DFTBAGeneratorPC/main/TemplateEditorWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace cn.zuoanqh.open.DFTBAGen.main
+{
+	/// <summary>
+	/// Interaction logic for TemplateEditorWindow.xaml
+	/// </summary>
+	public partial class TemplateEditorWindow : Window
+	{
+		public TemplateEditorWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ DFTBAGeneratorPC/test project/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace cn.zuoanqh.open.DFTBAGen.test_project
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
