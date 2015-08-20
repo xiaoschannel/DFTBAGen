6f9aadbfd724db08559e0c78b027fb39a2184f7a
左岸清河 <zuoanqh@live.com>
2015-08-20 08:04:17 +0800

    Processing word lists from MOBY and SCOWL, generating sentences
    Basic features implemented! None of the "template" stuff work atm. also
    don't support generate all possible (which is easy buy I'm lazy).
    Added copyright stuff for an early release.

--- DFTBAGeneratorPC/main/SimpleGeneratorWindow.xaml.cs
+++ DFTBAGeneratorPC/main/SimpleGeneratorWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using cn.zuoanqh.open.zut.FileIO.Text;
+using cn.zuoanqh.open.zut;
 
 namespace cn.zuoanqh.open.DFTBAGen.main
 {
@@ -19,9 +21,61 @@ namespace cn.zuoanqh.open.DFTBAGen.main
 	/// </summary>
 	public partial class SimpleGeneratorWindow : Window
 	{
+		Dictionary<string, List<string>> posLists;
+		List<string> verbB, nounA;
+		Random r = new Random();
 		public SimpleGeneratorWindow()
 		{
 			InitializeComponent();
+			posLists = new Dictionary<string, List<string>>();
+			posLists.Add("noun", new List<string>());
+			posLists.Add("verb", new List<string>());
+			posLists.Add("adjective", new List<string>());
+			posLists.Add("adverb", new List<string>());
+
+			foreach (var s in posLists.Keys)
+			{
+				posLists[s].AddRange(ByLineFileIO.readFileNoWhitespace("words\\"+s + ".txt"));
+			}
+			posLists["verb"] = posLists["verb"].FindAll((s) => (!s.EndsWith("ed") && !s.EndsWith("ing")));
+
+			verbB = posLists["verb"].FindAll((s) => s.StartsWith("b"));
+			nounA = posLists["noun"].FindAll((s) => s.StartsWith("a"));
+		}
+
+		private void Expander_Collapsed(object sender, RoutedEventArgs e)
+		{
+			this.Height = 210;
+		}
+
+		private void Expander_Expanded(object sender, RoutedEventArgs e)
+		{
+			this.Height = 299;
+		}
+
+		private void btnOne_Click(object sender, RoutedEventArgs e)
+		{
+			txtOutput.Clear();
+
+			string verb = verbB[r.Next(verbB.Count)];
+			string noun = nounA[r.Next(nounA.Count)];
+			txtOutput.Text += "Don't forget to " + verb + " " + noun;
+
+			Clipboard.SetText(txtOutput.Text);
+		}
+
+		private void btnMany_Click(object sender, RoutedEventArgs e)
+		{
+			txtOutput.Clear();
+
+			for (long i = 0; i < Convert.ToInt64(txtNumbers.Text); i++)
+			{
+				string verb = verbB[r.Next(verbB.Count)];
+				string noun = nounA[r.Next(nounA.Count)];
+				txtOutput.Text += "Don't forget to " + verb + " " + noun+"\n";
+			}
+
+			Clipboard.SetText(txtOutput.Text);
 		}
 	}
 }
---
+++ DFTBAGeneratorPC/word list processing/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using cn.zuoanqh.open.zut.FileIO.Text;
+using cn.zuoanqh.open.zut;
+
+namespace cn.zuoanqh.open.DFTBAGen.word_list_processing
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			StopWatch w = new StopWatch();
+
+			List<string> mobypos;
+			SortedSet<string> scowl35 = new SortedSet<string>();
+			w.Click();
+			Console.WriteLine("Reading files...");
+			mobypos = ByLineFileIO.readFileNoWhitespace("mobyposi.i");
+			foreach (var s in ByLineFileIO.readFileNoWhitespace("english-words.35"))
+				scowl35.Add(s);
+			Console.WriteLine("Finished! Took {0:F} ms.", w.Click());
+			Console.WriteLine("Now parsing...");
+			Dictionary<string, string> mobyParsed = new Dictionary<string, string>();
+			foreach(string s in mobypos)
+			{
+				try
+				{
+					var pts = zusp.CutFirst(s, "*");
+					if (!mobyParsed.ContainsKey(pts.First))
+						mobyParsed.Add(pts.First, pts.Second);
+					else
+						mobyParsed[pts.First] += pts.Second;
+				}
+				catch (Exception e)//I've had enough with encoding
+				{ continue; }
+
+			}
+			Console.WriteLine("Finished! Took {0:F} ms.", w.Click());
+			Console.WriteLine("Filtering the list with SCOWL and remove uncommon words...");
+			Dictionary<string, string> filtered = new Dictionary<string, string>();
+			foreach (string s in mobyParsed.Keys)
+			{
+				if (scowl35.Contains(s)) filtered.Add(s, mobyParsed[s]);
+			}
+			Console.WriteLine("Finished! Took {0:F} ms.", w.Click());
+			Console.WriteLine("Filtered list contains {0:D} words with part-of-speech.", filtered.Count);
+			Console.WriteLine("Generating part-of-speech lists...");
+			Dictionary<string, List<string>> posLists = new Dictionary<string, List<string>>();
+			posLists.Add("noun", new List<string>());
+			posLists.Add("verb", new List<string>());
+			posLists.Add("adjective", new List<string>());
+			posLists.Add("adverb", new List<string>());
+			foreach (var s in filtered.Keys)
+			{ 
+				foreach(char c in filtered[s].ToCharArray())
+				{
+					if (c == 'N' || c == 'r') posLists["noun"].Add(s);
+					if (c == 'V' || c == 't' || c == 'i') posLists["verb"].Add(s);
+					if (c == 'A') posLists["adjective"].Add(s);
+					if (c == 'v') posLists["adverb"].Add(s);
+				}
+			}
+			Console.WriteLine("Finished! Took {0:F} ms.", w.Click());
+			Console.WriteLine("Writing lists into files...");
+			foreach (var s in posLists.Keys)
+			{
+				Console.WriteLine("There were {0:D} in list {1}", posLists[s].Count,s);
+				ByLineFileIO.writeFile(posLists[s], s + ".txt");
+			}
+			Console.WriteLine("Finished! Took {0:F} ms. The end :)", w.Click());
+			Console.ReadLine();
+		}
+	}
+}
